05435a2694ed12f5f9f3d78989258401a824906d
kumahacker <kumahavana@gmail.com>
2019-10-24 00:15:22 +0200

    update

--- service.php
+++ service.php
@@ -7,9 +7,13 @@ class Service
 	 *
 	 * @param Request
 	 * @param Response
+	 *
+	 * @throws \Exception
 	 */
 	public function _main(Request $request, Response $response)
 	{
+		Challenges::complete('read-terminos', $request->person->id);
+
 		$response->setCache("month");
 		$response->setTemplate("home.ejs");
 	}
