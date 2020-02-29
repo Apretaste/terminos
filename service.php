be8e7007522dfacb65b3f3674084869c1e54f923
kumahacker <kumahavana@gmail.com>
2020-02-29 16:23:45 -0500

    fix challenges

--- service.php
+++ service.php
@@ -2,18 +2,23 @@
 
 use Apretaste\Request;
 use Apretaste\Response;
+use Apretaste\Challenges;
 
 class Service
 {
 	/**
 	 * Main function
 	 *
-	 * @param Request
-	 * @param Response
+	 * @param \Apretaste\Request  $request
+	 * @param \Apretaste\Response $response
+	 *
+	 * @throws \Framework\Alert
 	 */
 	public function _main(Request $request, Response $response)
 	{
-		$response->setCache("month");
+	  Challenges::complete('read-terminos', $request->person->id);
+
+	  $response->setCache("month");
 		$response->setTemplate("home.ejs");
 	}
 }
